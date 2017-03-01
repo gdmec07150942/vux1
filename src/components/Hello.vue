7a462aba2e5446b404b6a63bfecc6fe50716715f
Xufengtao <554061700@qq.com>
2017-03-01 10:10:02 +0000

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span class="title">欢迎登录</span>
+    <divider>Vue Vux</divider>
+    <Group>
+      <XInput title="账号" v-module='account' placeholder='账号' type='text'></XInput> 
+      
+    </Group>
+    <X-input title="密码" v-module='password' placeholder='密码' type="password"></X-input>
+    <XButton type="primary" action-type="button">登录</XButton>
+  </div>
+</template>
+
+<script>
+  import { Divider, Group, XInput, XButton } from 'vux'
+  export default {
+    components: {
+      Divider,
+      Group,
+      XInput,
+      XButton
+    },
+    data () {
+      return {
+        account: '',
+        password: ''
+      }
+    }
+  }
+</script>
